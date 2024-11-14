a9238b7c4196b75120cf09911d3035cd1d720f93
Thierry <130665360+Wiped-outt@users.noreply.github.com>
2024-11-14 08:54:01 -0300

    js

---
+++ static/JS/cadastro.js
@@ -0,0 +1,42 @@
+// Obter todos os dropdowns do documento
+const dropdowns = document.querySelectorAll('.dropdown');
+
+// Percorrer todos os elementos dropdown
+dropdowns.forEach(dropdown => {
+    // Obter elementos internos de cada dropdown
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+    // Adicionar um evento de clique ao elemento select
+    select.addEventListener('click', () => {
+        // Adicionar os estilos de clique ao elemento select
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        // Adicionar os estilos de abertura ao elemento menu
+        menu.classList.toggle('menu-open');
+    });
+
+    // Percorrer todos os elementos de opção
+    options.forEach(option => {
+        // Adicionar um evento de clique ao elemento de opção
+        option.addEventListener('click', () => {
+            // Alterar o texto interno de "selected" para o texto interno da opção clicada
+            selected.innerText = option.innerText;
+            // Remover os estilos de clique do elemento select
+            select.classList.remove('select-clicked');
+            // Remover os estilos de rotação do elemento caret
+            caret.classList.remove('caret-rotate');
+            // Remover os estilos de abertura do elemento menu
+            menu.classList.remove('menu-open');
+            // Remover a classe ativa de todos os elementos de opção
+            options.forEach(option => {
+                option.classList.remove('active');
+            });
+            // Adicionar a classe ativa ao elemento de opção clicado
+            option.classList.add('active');
+        });
+    });
+});
